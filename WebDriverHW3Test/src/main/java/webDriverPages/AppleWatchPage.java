a14fb7c92d1585ce9f4054a2c03ed66b4c0bb07d
Valentyna Leleko <108763534+lelekovg@users.noreply.github.com>
2023-06-09 02:15:58 +0300

    Add files via upload

---
+++ WebDriverHW3Test/src/main/java/webDriverPages/AppleWatchPage.java
@@ -0,0 +1,36 @@
+package webDriverPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.By.id;
+
+public class AppleWatchPage extends BasePage {
+
+    private static final String ADD_TO_CART_POPUP = "js_cart";
+    @FindBy(xpath = "//*[@id='js_cart']/div[2]/div[2]/a[1]")
+    private WebElement continueShoppingButton;
+
+    @FindBy(xpath = "//div[contains(@class,'prod-cart')]//a[(@class= 'prod-cart__buy')][1]")
+    private WebElement firstFieldWatch;
+
+    public AppleWatchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickBuyFirstWatchButton() {
+        firstFieldWatch.click();
+    }
+
+    public void clickContinueShoppingButton() {
+        continueShoppingButton.click();
+    }
+
+    public By getAddToCartPopup() {
+        return id(ADD_TO_CART_POPUP);
+    }
+
+
+}
---
+++ WebDriverHW3Test/src/test/java/webDriverTests/BaseTest.java
@@ -0,0 +1,56 @@
+package webDriverTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import webDriverPages.AppleStorePage;
+import webDriverPages.HomePage;
+import webDriverPages.AppleWatchPage;
+import webDriverPages.SearchPage;
+
+
+public class BaseTest {
+    WebDriver driver;
+    private static final String AVIC_URL = "https://avic.ua/";
+
+    @BeforeTest
+    public void profileSetup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+    }
+
+    @BeforeMethod
+    public void testSetup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(AVIC_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public HomePage getHomePage() {
+        return new HomePage(getDriver());
+    }
+
+    public AppleStorePage getAppleStorePage() {
+        return new AppleStorePage(getDriver());
+    }
+
+    public AppleWatchPage getAppleWatchPage() {
+        return new AppleWatchPage(getDriver());
+    }
+
+    public SearchPage getSearchPage() {
+        return new SearchPage(getDriver());
+    }
+
+
+}
